64539fc685409d528a3a5f72ffb030e1c4a7c23c
Diptanu Choudhury <diptanuc@gmail.com>
2013-12-09 11:11:11 -0800

    Added the script to create debian package

--- packaging/pack.sh
+++ packaging/pack.sh
@@ -4,6 +4,6 @@ rm -rf /tmp/usr/local/share/skyline
 
 mkdir -p /tmp/usr/local/share/skyline
 
-cp -r ../ /tmp/usr/local/share/skyline/
+cp -r . /tmp/usr/local/share/skyline/
 
 fpm -n skyline -t deb -s dir -v $1 -C /tmp usr/local/share/skyline
